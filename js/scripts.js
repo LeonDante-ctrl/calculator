b91107ef21c4aea1e2b06727076b5fcef52b8755
Dante.L <dantel@D.L>
2020-05-03 00:17:33 +0300

    Addition code implemented on my js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+  return number1 + number2 ;
+};
+
+var number1 = parseInt(prompt("Enter number here:"));
+var number2 = parseInt(prompt("Enter other other number:"));
+var result = add(number1, number2);
+alert(result);
