4645d94b14fc3f722dec4c96aa168b6e60565861
Dante.L <dantel@D.L>
2020-05-03 00:39:20 +0300

    Added code for division

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
+var divide = function(number1, number2) {
+  return number1 / number2;
 };
 
 var number1 = parseInt(prompt("Enter number here:"));
 var number2 = parseInt(prompt("Enter other number here:"));
 
-var result = multiply(number1, number2);
+var result = divide(number1, number2);
 alert (result);
