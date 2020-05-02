fa3c60a1142c2c305df467b97411ec886aadf625
Dante.L <dantel@D.L>
2020-05-03 00:35:12 +0300

    Added code for subtraction

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
-var add = function(number1, number2) {
-  return number1 + number2 ;
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
 };
 
 var number1 = parseInt(prompt("Enter number here:"));
-var number2 = parseInt(prompt("Enter other other number:"));
-var result = add(number1, number2);
-alert(result);
+var number2 = parseInt(prompt("Enter other number here:"));
+
+var result = subtract(number1, number2);
+alert (result);
