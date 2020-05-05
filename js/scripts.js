fbc1f479ec6c39f6b4ed919593c4a9921d322366
Dante.L <dantel@D.L>
2020-05-05 10:34:18 +0300

    Here is the implementation of basic functions on the calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 var divide = function(number1, number2) {
-  return number1 / number2;
+  return  number1 / number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var subtract = function(number1, number2) {
+  return number1 - number2;
 };
 
 var number1 = parseInt(prompt("Enter number here:"));
 var number2 = parseInt(prompt("Enter other number here:"));
 
-var result = divide(number1, number2);
-alert (result);
+ var result = function(number1, number2) //Enter function to implement to both number1 and number2;
+ alert ("The answer to your question is " + result);
+
+ 
